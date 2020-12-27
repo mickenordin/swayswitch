eccf3a8f583db3d4e780b36214a3d741c9fcb44d
Micke Nordin <mik@elnord.in>
2020-12-27 16:18:07 +0100

    Add cleanup script

---
+++ packaging_scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+BASEDIR=~/sources
+cd ${BASEDIR}/swayswitch
+rm -r  debian/.debhelper/ debian/debhelper-build-stamp debian/files debian/swayswitch.substvars debian/swayswitch/
--- packaging_scripts/run.sh
+++ packaging_scripts/run.sh
@@ -3,3 +3,4 @@ BASEDIR=~/sources
 ${BASEDIR}/swayswitch/packaging_scripts/version.sh
 ${BASEDIR}/swayswitch/packaging_scripts/reprepro.sh
 ${BASEDIR}/swayswitch/packaging_scripts/alien.sh
+${BASEDIR}/swayswitch/packaging_scripts/cleanup.sh
