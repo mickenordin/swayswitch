1c65cf7370155b34d860dbb88adfe2c7a8e9c3ff
Micke Nordin <kano@sunet.se>
2021-07-08 11:19:41 +0200

    Moving to smolnet.org

--- packaging_scripts/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-BASEDIR=~/sources
-cd ${BASEDIR}/swayswitch
-rm -r  debian/.debhelper/ debian/debhelper-build-stamp debian/files debian/swayswitch.substvars debian/swayswitch/
--- packaging_scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-BASEDIR=~/sources
-${BASEDIR}/swayswitch/packaging_scripts/version.sh
-${BASEDIR}/swayswitch/packaging_scripts/reprepro.sh
-${BASEDIR}/swayswitch/packaging_scripts/alien.sh
-${BASEDIR}/swayswitch/packaging_scripts/cleanup.sh
